4dfce975ff56c32bbb9f2556ca3adeb107493fef
Choriz0 <96111521+Choriz0id@users.noreply.github.com>
2025-10-19 18:58:21 +0200

    Update config.php

--- user/config.php
+++ user/config.php
@@ -19,6 +19,8 @@ $yourls_user_passwords = [
 ];
 $yourls_reserved_URL = [];
 
+define('YOURLS_ADMIN', true); // должна быть включена
+
 // **Cookie key** - случайная строка для безопасности
 define( 'YOURLS_COOKIEKEY', 'r8UjW9sE2zTQ4bP1xC6kL0vF3aYdM7nH' );
 
